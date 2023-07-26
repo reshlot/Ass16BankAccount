c5e214bcc48d0e7d1c9f5b03720e05f56ecf5313
HP <reshmaksriet@gmail.com>
2023-07-26 15:30:00 +0530

    Initial commit

---
+++ BankAcccount.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ass16BankAccount
+{
+    public class BankAccount
+    {
+        public int AccountNumber { get; }
+        public string AccountHolderName { get; set; }
+        private decimal Balance { get; set; }
+
+        // Constructor
+        public  BankAccount (string accountHolderName)
+        {
+            AccountNumber = 723456;
+            AccountHolderName = accountHolderName;
+            Balance = 0;
+        }
+
+        // Methods
+        public void Deposit(decimal amount)
+        {
+            Balance += amount;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            if (amount > Balance)
+            {
+                Console.WriteLine("Insufficient balance -> :(\n");
+            }
+            else
+            {
+                Balance -= amount;
+            }
+        }
+
+        
+        public void PrintAccountDetails()
+        {
+            Console.WriteLine("****Current Account Details****\n");
+           // Console.WriteLine("=================================");
+            Console.WriteLine("Account Number: "+AccountNumber);
+            Console.WriteLine("Account Holder Name: " + AccountHolderName);
+            Console.WriteLine(" Current Balance: " + Balance);
+            Console.WriteLine();
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ass16BankAccount
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Create an instance of BankAccount
+            BankAccount account = new BankAccount("Michael Jackson");
+
+            //BankAccount Details
+            Console.WriteLine("****Account Details****\n");
+            Console.WriteLine("Account Number:723456");
+            Console.WriteLine("Account Holder Name:Michael Jackson");
+            Console.WriteLine("Balance:0\n");
+
+
+
+
+
+            // Deposit some amount
+            Console.WriteLine("=================================");
+            Console.WriteLine("Deposit Rs.12000/-!!!");
+            Console.WriteLine("=================================");
+            account.Deposit(12000);
+            account.PrintAccountDetails();
+
+            // Withdraw some amount
+            Console.WriteLine("=================================");
+            Console.WriteLine("Withdraw Rs.5000/-!!!");
+            Console.WriteLine("=================================");
+            account.Withdraw(5000);
+            account.PrintAccountDetails();
+
+            
+            Console.ReadKey();
+        }
+    }
+}
+
